72387ad6c23b014e3baadad6ffb49d7101ca3cfe
杨铭 <ym@ymdeiMac.local>
2017-05-15 10:29:16 +0800

    修改文件
    修改文件

--- 树状图上中下/Macro/ConstantDefine.h
+++ 树状图上中下/Macro/ConstantDefine.h
@@ -10,11 +10,6 @@
 #ifndef ConstantDefine_h
 #define ConstantDefine_h
 
-#define AppGradeVersion             [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"]//Bundle版本号
-#define AppVersion                                                          @"1.0.1"                        //app版本号
-
-#define DESSTR  @"8798dfdda8c4ea8114ff3059bf5dea30"//加密串
-#define AppName @"家族谱"                             //应用名称
 #define IOS_VERSION_7_OR_ABOVE (([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0)? (YES):(NO))
 #define IOS_VERSION_8_OR_ABOVE (([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)? (YES):(NO))
 #define SystemLanguage         [[NSLocale preferredLanguages] objectAtIndex:0]           //系统语言
@@ -50,8 +45,6 @@
 #define NAVIBAR_BG [SUtils           imageWithColor:NavibarColor]
 
 
-#define THEME_COLOR             RGB(24, 128, 251)         //项目主题色
-
 #define BigTextFontSize [UIFont fontWithName:FontStyle  size:30]
 #define TitleFontSize [UIFont fontWithName:FontStyle    size:20]
 #define TextFontSize [UIFont fontWithName:FontStyle     size:15]
@@ -63,7 +56,6 @@
 #define TagFontSize [UIFont fontWithName:FontStyle      size:30]
 #define BoldFont(r) [UIFont                             boldSystemFontOfSize:r]
 
-#define UILABEL_LINE_SPACE 6 //Label内文字的行间距
 
 #define MarginLeft       15
 #define MarginRight      15
@@ -80,45 +72,11 @@
 #define emptyIconWid 140 //无数据时显示的view中，图片的宽度
 #define CornerRadius 4
 
-#define MaxVoiceTime 60//最长录音时长限制,要用浮点数来计算
-
 #define HeadWidth 70
 #define HeadHeight (70+25)
 #define XSpace 30
 #define YSpace 60
 
-#define IsLogin [SUtils checkIsNil:[Utils getUserId]]//是否登录
-
-#define UserID [SUtils getUserId]//获取用户ID
-
-#define LoginUserInfo          @"LoginUserInfo"
-#define EditorDataInfo         @"EditorDataInfo"
-#define DetailUserInfo         @"DetailUserInfo"
-#define ChangeInfo             @"ChangeInfo"
-#define LoadLoginUser          @"LoadLoginUser"
-#define RefreshDemandView      @"RefreshDemandView"
-#define RerfeshMessage         @"RerfeshMessage"
-#define DidReceiveNotification @"DidReceiveNotification"
-#define WechatPaySuccess       @"WechatPaySuccess"
-
-
-#define UserIdKey         @"uid"
-#define UserDictionaryKey @"userDic"
-/**
- *  页面缓存用的key，内存缓存，磁盘缓存
- */
-#define Cache_Index         @"index"   //首页，即族谱大厅
-#define Cache_Myspace       @"mySpace" //我的空间
-#define Cache_HomeTownHall  @"homeTownHall" //家乡话页面中的家乡话大厅
-#define Cache_NeedHall      @"needHall" //家乡话页面中的需求大厅
-#define Cache_CenterFirst   @"centerFirst" //会员中心首页
-#define Cache_CollectionHall   @"collectionhall" //收藏大厅
-#define Cache_PedigreeHall   @"pedigreehall" //族谱大厅
-#define Cache_HallOfFame   @"halloffame" //名人堂列表
-
-
-//宽度
-#define FloatRect(str,font)           [str boundingRectWithSize:CGSizeMake(MainScreenW, 10000) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:font]} context:nil];
 
 
 #endif /* ConstantDefine_h */
